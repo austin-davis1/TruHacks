de910db6b219be0796ca91660a7d4f0ae08f0d4d
cartercameron1 <cartercameron864@gmail.com>
2024-03-24 00:45:06 -0500

    update req

--- backend/data.py
+++ backend/data.py
@@ -8,12 +8,14 @@ def plot_risk_of_outbreak(num_days = 30):
     num_days = 30
     risk_of_outbreak = np.random.randint(0, 100, size=num_days)
 
-    fig = plt.figure(figsize=(8, 6))
+    fig = plt.figure()
     obj = plt.plot(risk_of_outbreak, marker='o', color='r')
+
+
     plt.title('Risk of Outbreak')
     plt.xlabel('Days')
     plt.ylabel('Risk')
-    plt.grid(True)
+
     return mpld3.fig_to_html(fig)
 
 def plot_cases(num_days = 30):
@@ -21,40 +23,43 @@ def plot_cases(num_days = 30):
     cases = np.random.randint(0, 50, size=num_days)
     fig = plt.figure(figsize=(8, 6))
     obj = plt.plot(cases, marker='o', color='g')
+
     plt.title('Cases')
     plt.xlabel('Days')
     plt.ylabel('Number of Cases')
-    plt.grid(True)
+
     return mpld3.fig_to_html(fig)
 
 def plot_hospital_capacity(num_days = 30):
     current_hospital_capacity = np.random.randint(50, 200, size=num_days)
-    fig = plt.figure(figsize=(8, 6))
+    fig = plt.figure()
     obj = plt.plot(current_hospital_capacity, marker='o', color='b')
+
     plt.title('Current Hospital Capacity')
     plt.xlabel('Days')
     plt.ylabel('Capacity')
-    plt.grid(True)
+
     return mpld3.fig_to_html(fig)
 
 def plot_current_risk(num_days = 30):
 
     current_risk = np.random.rand(num_days) * 0.5 + 0.3
-    fig = plt.figure(figsize=(8, 6))
+    fig = plt.figure()
     obj = plt.plot(current_risk, marker='o', color='m')
+
     plt.title('Current Risk')
     plt.xlabel('Days')
     plt.ylabel('Risk')
-    plt.grid(True)
+
     return mpld3.fig_to_html(fig)
 
 def gen_table():
     d_table = {}
     d_table['Disease'] = ['Malaria', 'Aids', 'Ebola', 'Covid', 'Tuberculosis']
-    d_table['Count'] = [np.random.randint(1,1000),np.random.randint(1,1000),np.random.randint(1,1000),np.random.randint(1,1000),np.random.randint(1,1000)]
+    d_table['Count'] = [np.random.randint(100,1000),np.random.randint(1,1000),np.random.randint(1,1000),np.random.randint(1,1000),np.random.randint(1,1000)]
     d_table['Deaths'] = []
     for x in d_table['Count']:
-        d_table['Deaths'].append(x-np.random.randint(1,900))
+        d_table['Deaths'].append(x-np.random.randint(1,90))
     return json.dumps(d_table)
 
 
